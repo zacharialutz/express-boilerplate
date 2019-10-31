d05381cc4137f1cb0131ceba8490e22fe5b481ae
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-10-31 11:20:10 -0500

    formatting edit

--- src/app.js
+++ src/app.js
@@ -12,12 +12,13 @@ const morganOption = (NODE_ENV === 'production')
 	: 'common';
 
 app.use(morgan(morganOption));
-app.use(helmet())
-app.use(cors())
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Hello beautiful!');
 })
+
 app.use(function errorHandler(error, req, res, next) {
 	let response;
 	if (NODE_ENV === 'production') {
